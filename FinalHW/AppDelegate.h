0792721ee9b179fa273aad98f11a4986b0728a2d
Vthree <ji-ye@Ji-Yeteki-MacBook-Pro.local>
2014-05-30 18:53:05 +0800

    Initial Commit

---
+++ FinalHW/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalHW
+//
+//  Created by Ji-Ye on 14/5/30.
+//  Copyright (c) 2014年 vth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
